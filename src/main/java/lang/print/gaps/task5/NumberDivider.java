0cc4c1dec04f5e333a2b4c06d5b2333477e93e80
Ani <ani.mkrtchyan1982@gmail.com>
2023-03-04 15:59:59 +0400

    Operators

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,13 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        double result = toBeDivided / 5.0;
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        NumberDivider numberDivider = new NumberDivider();
+        numberDivider.divide(12);
+        numberDivider.divide(25);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,18 @@
 package lang.print.gaps.task5;
 
 public class TimeConvertor {
+
     public void convert(float minutes) {
+        double second = minutes * 60;
+        System.out.println(second);
+    }
+
+    public static void main(String[] args) {
+        TimeConvertor timeConvertor = new TimeConvertor();
+        timeConvertor.convert(3);
     }
 }
+
+
+
+
